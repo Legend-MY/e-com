d22741a12f92bedf886481a0c2e091e2c88e4df3
Legend-MY <markov2008@bigmir.net>
2021-03-28 23:45:48 +0300

    Сделал шапку + адаптив

--- #src/js/mobilecheck.js
+++ #src/js/mobilecheck.js
@@ -29,6 +29,16 @@
    if (isMobile.any()) {
       document.body.classList.add('touch');
 
+      let menuArrows = document.querySelectorAll('.menu__link.arrow');
+      if (menuArrows.length > 0) {
+         for (let i = 0; i < menuArrows.length; i++) {
+            const menuArrow = menuArrows[i];
+            menuArrow.addEventListener('click', function (event) {
+               menuArrow.parentElement.classList.toggle('active');
+            });
+         }
+      }
+
    } else {
       document.body.classList.add('pc');
    }
--- #src/js/script.js
+++ #src/js/script.js
@@ -1,43 +1,52 @@
-'use strict';
+'use strict'
 
 document.addEventListener('DOMContentLoaded', function () {
-
-   @@include('webpcheck.js'); // Проверка поддерживает ли браузер webP
-   // @@include('mobilecheck.js'); // Определение с какого устройства зашел пользователь
-   // @@include('swiper-bundle.min.js'); // Swiper slider
-
-
-   /* Селекторы языка и валюты */
-
-   /* ======================================== */
-
-
-   /* Попап поиска */
-
-   /* ======================================== */
+   /* Проверка поддерживает ли браузер webP */
+   @@include('webpcheck.js');
+   /* ===================================== */
+   /* Проверка с какого устройства зашел пользователь */
+   @@include('mobilecheck.js');
+   /* ===================================== */
+   /* Подключение swiper slider */
+   @@include('swiper-bundle.min.js');
+   /* ===================================== */
+   /* Подключение popups */
+   @@include('popups.js');
+   /* ===================================== */
 
 
-   /* Выпадающее меню с категориями */
+   // Header selectors ====================================
+   (function () {
+      const selectorsBlock = document.querySelectorAll('.selectors__block');
 
-   /* ======================================== */
-
-   /* Меню бургер */
-   const burgerBtn = document.querySelector('.menu__burger'),
-      menuBody = document.querySelector('.menu__body');
-
-   if (burgerBtn) {
-      burgerBtn.addEventListener('click', function (event) {
-         document.body.classList.toggle('lock');
-         burgerBtn.classList.toggle('active');
-         menuBody.classList.toggle('active');
+      selectorsBlock.forEach(function (item) {
+         item.addEventListener('click', function (event) {
+            const target = event.target;
+            if (target && target.classList.contains('selectors__current')) {
+               item.querySelector('.selectors__options').classList.toggle('active');
+               item.querySelector('.selectors__current').classList.toggle('active');
+            }
+         });
       });
-   }
-   /* ======================================== */
+   })();
+   // ========================================================================
 
 
-   /* Swiper slider */
+   // Меню бургер ====================================
+   (function () {
+      const burgerBtn = document.querySelector('.menu__burger'),
+         menuBody = document.querySelector('.menu__body'),
+         body = document.querySelector('body');
 
-   /* ======================================== */
+      if (burgerBtn) {
+         burgerBtn.addEventListener('click', function (event) {
+            body.classList.toggle('lock');
+            burgerBtn.classList.toggle('active');
+            menuBody.classList.toggle('active');
+         });
+      }
+   })();
+   // ========================================================================
 
 
 });
